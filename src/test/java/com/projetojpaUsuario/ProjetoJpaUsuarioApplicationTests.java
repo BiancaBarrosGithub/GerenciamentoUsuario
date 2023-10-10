22db6621769db19ec97d4d716dd673531482a662
BiancaBarrosGithub <146129798+BiancaBarrosGithub@users.noreply.github.com>
2023-10-10 10:42:25 -0300

    Add files via upload

---
+++ src/test/java/com/projetojpaUsuario/ProjetoJpaUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetojpaUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJpaUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
